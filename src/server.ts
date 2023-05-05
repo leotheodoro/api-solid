44fd32e51faac64f9bc9e954a25baa9b3a55c622
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-05-05 13:51:09 -0300

    :construction: add user

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from '@/app'
 import { env } from '@/env'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 app
   .listen({
@@ -9,3 +11,23 @@ app
   .then(() => {
     console.log('🚀 HTTP Server running...')
   })
+
+app.post('/users', async (request, reply) => {
+  const registerUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerUserBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})

f16bcdc90291048c2a6f567997fe247e4b61ebf0
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-05-26 13:37:20 -0300

    feat: global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from '@/app'
 import { env } from '@/env'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
 
 app
   .listen({
@@ -12,3 +13,20 @@ app
   })
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Log to an external tool
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})

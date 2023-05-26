3c253e79a797725c25e0db26a3b2adf9e020d76b
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-05-26 10:51:37 -0300

    chore: controllers and separation of folders

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,21 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerUserBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from '@/app'
 import { env } from '@/env'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
+import { appRoutes } from './http/routes'
 
 app
   .listen({
@@ -12,22 +11,4 @@ app
     console.log('🚀 HTTP Server running...')
   })
 
-app.post('/users', async (request, reply) => {
-  const registerUserBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = registerUserBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.register(appRoutes)

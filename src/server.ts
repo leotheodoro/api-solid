0a8717a84ffc8b6ed037cc88e98df6e1c7b77afb
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-08-13 22:22:17 -0300

    ✅ register e2e

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,5 @@
 import { app } from '@/app'
 import { env } from '@/env'
-import { appRoutes } from './http/routes'
-import { ZodError } from 'zod'
-import fastifyJwt from '@fastify/jwt'
-
-app.register(fastifyJwt, {
-  secret: env.JWT_SECRET,
-})
-
-app.register(appRoutes)
-
-app.setErrorHandler((error, _request, reply) => {
-  if (error instanceof ZodError) {
-    return reply.status(400).send({
-      message: 'Validation error.',
-      issues: error.format(),
-    })
-  }
-
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
-  } else {
-    // TODO: Log to an external tool
-  }
-
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
 
 app
   .listen({
